007f068d12f61360029a5364139999c2a6a80996
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2023-07-25 22:11:34 +0300

    Добавление роутинга, приватных маршрутов. Создание страницы 404

--- src/const.tsx
+++ src/const.tsx
@@ -2,3 +2,16 @@ export const Setting = {
   PlacesCount: 312,
   PlacesOnPage: 5,
 };
+
+export const AppRoute = {
+  Favorites: '/favorites',
+  Login: '/login',
+  Main: '/',
+  Offer: '/offer/:id'
+};
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
